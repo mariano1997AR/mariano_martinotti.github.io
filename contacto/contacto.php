70d8fcbcde79213645f237fbff3c0b2332447041
Mariano <mariano.andres1024@gmail.com>
2023-09-04 17:06:36 -0300

    borramos contacto.html y bd.php, usamos directamente un solo archivo de contactocon las configuraciones y la pagina

--- contacto/contacto.php
+++ contacto/contacto.php
@@ -26,16 +26,38 @@
  $telefono = $_POST['telefono'];
  $comentario = $_POST['comentarios'];
  if (isset($email) && isset($telefono) && isset($comentario)) {
-    /*
-    echo $email . "<br>";
-    echo $telefono . "<br>";
-    echo $comentario . "<br>";
-    */
     insertarDatos($conn,$email,$telefono,$comentario);
-    header("Location: http://localhost/cv_page/contacto/contacto.html");
 }else{
     echo "Los campos estan vacios";
  }
 
  mysqli_close($conn); 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto - Mariano andres Martinotti</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="shortcut icon" href="../img/logo_martinotti.png" type="image/x-icon">
+    <link rel="stylesheet" href="../css/form.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/contacto.css">
+</head>
+<body>
+    <div class="container mt-3">
+        <div class="mt-5 p-5 bg-dark text-white rounded"> 
+            <h2>Vamos a direccionar la pagina...</h2>
+            <p>Recibi su mensaje a la brevedad le estare respondiendo....</p>
+        </div>
+      </div>
+
+    <!-- BOOSTRAPS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- REDIRECCIONAMOS -->
+    <script src="../js/redireccionar.js"></script>
+</body>
+</html>
